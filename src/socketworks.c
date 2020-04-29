c7f54ffbd5b267e54f65cb3b26b6a38f1c449786
Lars The <lars18th@users.noreply.github.com>
2020-04-29 11:13:27 +0200

    Fix initialization of flags in sockets

--- src/socketworks.c
+++ src/socketworks.c
@@ -605,6 +605,7 @@ int sockets_add(int sock, USockAddr *sa, int sid, int type,
 	ss->lbuf = 0;
 	ss->timeout_ms = 0;
 	ss->id = i;
+	ss->flags = 0;
 	ss->sock_err = 0;
 	ss->overflow = 0;
 	ss->buf_alloc = ss->buf_used = 0;
